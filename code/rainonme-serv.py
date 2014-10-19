dc6588db3a903146a9ec7ae258495f9cf74b9dee
Derek M. Jones <plagchk@Joes-MacBook-Pro.local>
2014-10-19 02:00:48 +0100

    Work out station that future weather is coming from

--- code/rainonme-serv.py
+++ code/rainonme-serv.py
@@ -2,6 +2,7 @@
 # raininme-serv.py, 18 Oct 14
 #
 
+import json
 from json import loads
 from urllib2 import urlopen
 from datetime import datetime, date, timedelta
@@ -17,6 +18,8 @@ def get_cur_location():
 # The client passes us current lat/long, hard code TechCrunch dungeon location for now
    return [51.508907, -0.084054]
 
+def blow_weather(cur_lat_long, wind_dir, london_station):
+   return []
 
 def rain_prediction():
 
@@ -47,14 +50,28 @@ def rain_prediction():
 # Save on our daily limit of API calls by reusing data that is unlikely to change
 
    london_f = open('../data/london-station.json')
-   london_station=london_f.read()
+   london_station=json.load(london_f)
+   london_f.close()
 
-# Find nearest station to us
-   closest=99999
+# Find closest station to us
+   cur_closest=99999
+   close_station=[]
    for l_s in london_station:
+      s_dist=abs(cur_lat-l_s['Lat'])+abs(cur_long-l_s['Long'])
+      if (s_dist < cur_closest):
+         cur_closest=s_dist
+         close_station=l_s
+
+   # print cur_closest, close_station
 
 # http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KCASANTE9&graphspan=month&month=10&day=1&year=2012&format=1
 
+#   closest_data = ''.join(['http://api.wunderground.com/api/', our_key, '/history_', date_time.strftime('%Y%m%d'), '/q/', l_s['id'], '.json'])
+
+# Find station(s) from where the wind is blowing the rain our way
+#   wind_dir=closest_data['wind_dir']
+
+   rain_from=blow_weather(cur_lat_long, wind_dir, london_station)
 
 #   x,y=[],[]
 #
@@ -71,8 +88,12 @@ def rain_prediction():
 
 # Test data
    rain_f = open('../data/rainonme.json')
-   rain_pred=rain_f.read()
-   return rain_pred
+   rain_pred=json.load(rain_f)
+   rain_f.close()
+
+# Make the json data a viable javascript assignment!!!
+   r_str=''.join(['var data=', json.dumps(rain_pred), ';'])
+   return r_str
 
 if __name__ == '__main__':
    t=rain_prediction()
